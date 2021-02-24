333dca6dce9007dc37903f39c43d75820abc53cb
vuanh <vutuananh.hd89@gmail.com>
2021-02-24 22:57:08 +0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props){
+  return (
+    <header>
+      <h1> PMI {props.country} Chapter </h1>
+    </header>
+  );
+}
+
+function Body(props){
+  return (
+    <section>
+      <p>Connecting PM community in {props.country}</p>
+      <ul>
+        {props.divisions.map((division) => (<li>{division}</li>))}
+      </ul>
+    </section>
+  );
+}
+
+const divisions = [
+  "Membership",
+  "Outreach",
+  "Tech"
+];
+
+
+
+function Footer(props){
+  return (
+    <footer>
+      <p>copyright {props.year}</p>
+    </footer>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header country="Indonesia" />
+      <Body country="Indonesia" divisions={divisions}/>
+      <Footer year= {new Date().getFullYear()}/>
     </div>
   );
 }
